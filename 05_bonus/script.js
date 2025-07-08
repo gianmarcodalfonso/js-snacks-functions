229502eeedc737e599913cbd152fbf5f50ab293d
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-08 16:19:08 +0200

    feat: 05_bonus>script arrowFunction && calling

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,26 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nname = 'Mario';
 
 
 // Dichiara la funzione qui.
+const dailyHello = (name) => {
+  let currentHour = new Date().getHours()
+  if (currentHour <= 13 && currentHour >= 0 ) {
+    console.log(`Buongiorno ${name}`)
+  }
+  else if (currentHour > 13 && currentHour <= 17) {
+    console.log(`Buon pomeriggio ${name}`)
+  }
+  else if (currentHour <= 23 && currentHour > 17) {
+    console.log(`Buonasera ${name}`)
+  }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+dailyHello(nname);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
