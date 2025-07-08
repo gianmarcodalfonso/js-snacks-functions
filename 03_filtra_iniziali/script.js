9d42ab35f03ba1af670d597a647b1cc05e0b897a
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-08 15:10:00 +0200

    feat: 03>script arrowFunction

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const startLetterCheck = (list, letter) => {
+  const checkedNames = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].charAt(0).toLowerCase() === letter.toLowerCase() ) {
+      checkedNames.push(list[i])
+    }
+  }
+  return checkedNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
