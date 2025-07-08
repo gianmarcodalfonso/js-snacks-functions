e5d536bb6410292a60f924189e1c18ad58e6dd1d
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-08 15:58:04 +0200

    feat: 04>script arrowFunction && calling

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+const countVowels = (word) => {
+  let vowelsCount = []
+  let wordArray = word.toLowerCase().split(``)
+  for(let i = 0; i < wordArray.length; i++) {
+    
+    if(wordArray[i] === `a` || wordArray[i] === `e` || wordArray[i] === `i` || wordArray[i] === `o` || wordArray[i] === `u`){
+      vowelsCount.push(wordArray[i])
+    }
+  }
+  return vowelsCount.length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(countVowels(word));
 
 
 
